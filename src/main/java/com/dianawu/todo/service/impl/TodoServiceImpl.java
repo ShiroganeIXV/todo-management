52b0f3da5d74d6e317c74172cfb7ab40ff180a0c
ShiroganeIXV <wudiana.124010121105@vtc.edu.vn>
2024-02-16 14:46:52 +0700

    Update Dto-Entity Mapping using ModelMapper

--- src/main/java/com/dianawu/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/dianawu/todo/service/impl/TodoServiceImpl.java
@@ -5,6 +5,7 @@ import com.dianawu.todo.entity.Todo;
 import com.dianawu.todo.repository.TodoRepository;
 import com.dianawu.todo.service.TodoService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,26 +13,32 @@ import org.springframework.stereotype.Service;
 public class TodoServiceImpl implements TodoService {
 
     private TodoRepository todoRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
 
-        // convert DTO to entity
+        //! convert DTO to entity
+        /*
         Todo todo = new Todo();
         todo.setTitle(todoDto.getTitle());
         todo.setDescription(todoDto.getDescription());
-        todo.setCompleted(todoDto.isCompleted());
+        todo.setCompleted(todoDto.isCompleted());*/ //Deprecated - Now using ModelMapper
 
+        Todo todo = modelMapper.map(todoDto, Todo.class);
 
-        // Persist entity to database
+        //! Persist entity to database
         Todo savedTodo = todoRepository.save(todo);
 
-        // Convert save Todo JPA entity to DTO
+        //! Convert save Todo JPA entity to DTO
+        /* Deprecated - Now using ModelMapper
+
         TodoDto savedTodoDto = new TodoDto();
         savedTodoDto.setId(savedTodo.getId());
         savedTodoDto.setTitle(savedTodo.getTitle());
         savedTodoDto.setDescription(savedTodo.getDescription());
-        savedTodoDto.setCompleted(savedTodo.isCompleted());
+        savedTodoDto.setCompleted(savedTodo.isCompleted());*/ // Deprecated - Now using ModelMapper
+        TodoDto savedTodoDto = modelMapper.map(savedTodo, TodoDto.class);
 
         return savedTodoDto;
     }
